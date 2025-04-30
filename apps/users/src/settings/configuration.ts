3a88281717c2f8e0b0169b152bbd53463370dac1
idogmat <idogmat@gmail.com>
2025-04-30 18:04:41 +0300

    first deploy

---
+++ apps/users/src/settings/configuration.ts
@@ -0,0 +1,22 @@
+export type EnvironmentsTypes =
+  | 'DEVELOPMENT'
+  | 'STAGING'
+  | 'PRODUCTION'
+  | 'TESTING';
+export const EnvironmentMode = {
+  DEVELOPMENT: 'DEVELOPMENT',
+  STAGING: 'STAGING',
+  PRODUCTION: 'PRODUCTION',
+  TESTING: 'TESTING',
+};
+export const Environments = Object.keys(EnvironmentMode);
+
+export const getConfiguration = () => {
+  console.log(process.env.NODE_ENV?.trim(), 'NODE_ENV');
+  return {
+    NODE_ENV: (Environments.includes(process.env.NODE_ENV?.trim())
+      ? process.env.NODE_ENV.trim()
+      : 'DEVELOPMENT') as EnvironmentsTypes,
+    PORT: process.env.PORT || process.env.USERS_PORT,
+  };
+};
--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
+import { getConfiguration } from './settings/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [getConfiguration],
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
