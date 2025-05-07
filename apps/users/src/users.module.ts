0252ebcd09c3823494e3e4b2b8cf9f8e41e72974
Ivan Ivanov <backtobackend@proton.me>
2025-05-07 12:17:27 -0700

    T-1 feat: add getEnvFilePath function that return specific env file depending on NODE_ENV

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -2,18 +2,40 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { ConfigModule } from '@nestjs/config';
-import { getConfiguration } from './settings/configuration';
+import {
+  EnvironmentMode,
+  EnvironmentsTypes,
+  getConfiguration,
+} from './settings/configuration';
 import { GraphQLModule } from '@nestjs/graphql';
 import {
   ApolloFederationDriver,
   ApolloFederationDriverConfig,
 } from '@nestjs/apollo';
 import { UsersResolver } from './users.resolver';
+
+const getEnvFilePath = (env: EnvironmentsTypes) => {
+  const defaultEnvFilePath = [
+    'apps/users/src/.env.development',
+    'apps/users/src/.env',
+  ];
+  if (env === EnvironmentMode.TESTING) {
+    console.log('testing');
+
+    return ['apps/users/src/.env.test', ...defaultEnvFilePath];
+  }
+
+  return defaultEnvFilePath;
+};
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [getConfiguration],
+      ignoreEnvFile:
+        process.env.NODE_ENV !== EnvironmentMode.DEVELOPMENT &&
+        process.env.NODE_ENV !== EnvironmentMode.TESTING,
+      envFilePath: getEnvFilePath(process.env.NODE_ENV as EnvironmentsTypes),
     }),
     GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       driver: ApolloFederationDriver,
