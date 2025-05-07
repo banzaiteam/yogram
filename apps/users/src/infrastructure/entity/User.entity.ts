98083af1fec249ae20f5886b5f6a3102231161cd
Ivan Ivanov <backtobackend@proton.me>
2025-05-07 16:42:54 -0700

    T-1 feat: add User entity and Profile enity with oneByOne relations. add connect to neon database

---
+++ apps/users/src/infrastructure/entity/User.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity } from 'apps/libs/common/entity/base.entity';
+import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
+import { Profile } from './Profile.entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @Column({ type: 'varchar', length: 30 })
+  firstName: string;
+  @Column({ type: 'varchar', length: 30 })
+  lastName: string;
+  @Column({ type: 'date' })
+  birthdate: Date;
+  @Column({ type: 'varchar', unique: true })
+  email: string;
+  @Column({ type: 'varchar', length: 30 })
+  password: string;
+  @Column({ type: 'text', nullable: true })
+  description: string;
+  @Column({ type: 'varchar' })
+  country: string;
+  @Column({ type: 'varchar' })
+  city: string;
+  @Column({ type: 'boolean' })
+  verified: boolean;
+
+  @OneToOne(() => Profile, (profile) => profile.user, { eager: true })
+  profile: Profile;
+}
