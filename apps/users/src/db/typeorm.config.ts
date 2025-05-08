80532bd7267ac09b308a7b96533800ac1ea9d3f0
Ivan Ivanov <backtobackend@proton.me>
2025-05-08 13:01:42 -0700

    T-1 feat: add users migration

---
+++ apps/users/src/db/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+//todo cant get configService
+export default new DataSource({
+  type: 'postgres',
+  host: 'ep-lively-bush-a2mv49a9-pooler.eu-central-1.aws.neon.tech',
+  port: 5432,
+  username: 'neondb_owner',
+  password: 'npg_RqNihtGd54IJ',
+  database: 'users',
+  entities: [`${__dirname}/../infrastructure/**/*.entity{.ts,.js}`],
+  migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+  extra: { ssl: true },
+  synchronize: false,
+  migrationsTableName: 'migrations',
+});
