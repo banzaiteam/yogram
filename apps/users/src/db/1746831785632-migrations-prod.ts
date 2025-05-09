59d936fe99381e3c87a2f45c854fa9fd10d54a4d
Ivan Ivanov <backtobackend@proton.me>
2025-05-09 16:07:49 -0700

    T-2 chore: add  prod migration commands to package.json

---
+++ apps/users/src/db/1746831785632-migrations-prod.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrationsProd1746831785632 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
