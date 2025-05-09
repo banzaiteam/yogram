73aeec3a8d5d83fd008aa12c6dbd8dd2cde4bcf7
Ivan Ivanov <backtobackend@proton.me>
2025-05-08 18:18:10 -0700

    T-2 feat: add Profile and User models

--- apps/libs/common/abstract/sql-query-base-repository.abstract.ts
+++ apps/libs/common/abstract/sql-query-base-repository.abstract.ts
@@ -2,13 +2,13 @@ import { Repository } from 'typeorm';
 import { BaseEntity } from '../entity/base.entity';
 import { AggregateRoot } from '@nestjs/cqrs';
 import { IModelEntityConvert } from '../interface/model-entity-convert.interface';
-import { RpcException } from '@nestjs/microservices';
 import { IQueryRepository } from '../interface/query-repository.interface';
 
 /**
  * Abstract method which implements base query sql crud methods.
  *
- * @param {AggregateRoot} TModel - Business model type that extends from cqrs(ddd) AggregateRoot class which allows dispatching events within model.
+ * @param {AggregateRoot} TModel - Business model type that extends from cqrs(ddd) AggregateRoot class
+ * which allows dispatching events within model.
  * @param {BaseEntity} TEntity - Each dataBase entity should extends from BaseEntity
  */
 export abstract class SqlQueryBaseRepository<
