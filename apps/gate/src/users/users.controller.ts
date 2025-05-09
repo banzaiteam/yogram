423f5443aef5f35c62b39082cad1f6dbeb0b2dde
Ivan Ivanov <backtobackend@proton.me>
2025-05-09 10:23:05 -0700

    T-2 feat: add route from gate users to users microservice

--- apps/gate/src/users/users.controller.ts
+++ apps/gate/src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): void {
+    this.usersService.create(createUserDto);
+  }
 }
