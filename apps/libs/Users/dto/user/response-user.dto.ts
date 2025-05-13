5db9f13715eb3bfec145a66d3051bce31c25f1a7
Ivan Ivanov <backtobackend@proton.me>
2025-05-12 21:31:31 -0700

    T-6 feat: add responseUserDto and UserQueryRepository with findOne method

---
+++ apps/libs/Users/dto/user/response-user.dto.ts
@@ -0,0 +1,19 @@
+import { Exclude, Expose } from 'class-transformer';
+
+@Exclude()
+export class ReponseUserDto {
+  @Expose()
+  id: string;
+  @Expose()
+  firstName: string;
+  @Expose()
+  lastName: string;
+  @Expose()
+  username: string;
+  @Expose()
+  birthdate: Date;
+  @Expose()
+  email: string;
+  @Expose()
+  country: string;
+}
