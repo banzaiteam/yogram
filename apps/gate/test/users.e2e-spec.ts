9c9d51617ea8a9080f321819fc4cbc4649ea06be
Ivan Ivanov <backtobackend@proton.me>
2025-05-14 13:21:38 -0700

    BAN-23 fix: some tests fixes but still not work)

--- apps/gate/test/users.e2e-spec.ts
+++ apps/gate/test/users.e2e-spec.ts
@@ -1,18 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
+import { UsersModule as UsersAppModule } from '../../users/src/users.module';
+import { UsersModule } from '../../gate/src/users/users.module';
 import { AppModule } from '../src/app.module';
 
 const createUserDto = {
-  firstName: 'Ivans',
-  lastName: 'Ivanov',
   username: 'mmm',
   email: 'retouch226@gmail.com',
-  'description:': 'fffff',
   password: '12O!k',
-  birthdate: '10/30/20',
-  city: 'Khm',
-  country: 'Prague',
 };
 
 describe('Users microservice (e2e)', () => {
@@ -21,16 +17,15 @@ describe('Users microservice (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
   // localhost:3001/api/v1/
   it.skip('/ (POST)', async () => {
     const result = await request(app.getHttpServer())
-      .post('/users/create')
+      .post('/signup')
       .send(createUserDto)
       .expect(201);
   });
--- apps/users/src/infrastructure/repository/query/user-query.repository.ts
+++ apps/users/src/infrastructure/repository/query/user-query.repository.ts
@@ -1,10 +1,10 @@
-import { BaseRepository } from 'apps/libs/common/abstract/base-repository.abstract';
-import { ResponseUserDto } from 'apps/libs/Users/dto/user/response-user.dto';
-import { IUsersQueryRepository } from 'apps/users/src/interfaces/query/user-query.interface';
+import { BaseRepository } from '../../../../../../apps/libs/common/abstract/base-repository.abstract';
+import { ResponseUserDto } from '../../../../../../apps/libs/Users/dto/user/response-user.dto';
+import { IUsersQueryRepository } from '../../../../../../apps/users/src/interfaces/query/user-query.interface';
 import { EntityManager, Repository } from 'typeorm';
 import { User } from '../../entity/User.entity';
 import { plainToInstance } from 'class-transformer';
-import { IdDto } from 'apps/libs/common/dto/id.dto';
+import { IdDto } from '../../../../../../apps/libs/common/dto/id.dto';
 
 export class UserQueryRepository
   extends BaseRepository
