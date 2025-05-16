0a2dd645af25c343528ca812055badf74c7c6622
Ivan Ivanov <backtobackend@proton.me>
2025-05-16 15:57:12 -0700

    BAN-23 feat: add signup/emailVerify gate endpoint and service method that verify jwt token and send request to users/email-verify

--- apps/gate/src/signup/signup.service.ts
+++ apps/gate/src/signup/signup.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { GateService } from '../../../../apps/libs/gateService';
 import { HttpUsersPath } from '../../../../apps/libs/Users/constants/path.enum';
 import { CreateUserDto } from '../../../../apps/libs/Users/dto/user/create-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class SignupService {
-  constructor(private readonly gateService: GateService) {}
+  constructor(
+    private readonly gateService: GateService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<void> {
     await this.gateService.usersHttpServicePost(
@@ -13,5 +21,19 @@ export class SignupService {
       createUserDto,
       {},
     );
+  }
+
+  async emailVerify(token: string): Promise<void> {
+    let email: object;
+    try {
+      email = await this.jwtService.verifyAsync(token);
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    }
+    // await this.gateService.usersHttpServicePost(
+    //   HttpUsersPath.EmailVerify,
+    //   email['email'],
+    //   {},
+    // );
   }
 }
