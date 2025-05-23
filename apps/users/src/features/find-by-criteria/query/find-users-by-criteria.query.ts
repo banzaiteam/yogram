74c1b0440e8090535e7d80624828ce40b475664f
Ivan Ivanov <backtobackend@proton.me>
2025-05-22 17:32:20 -0700

    BAN-23 feat: add findUserByCriteria to usersQueryRepository

--- apps/users/src/features/find-by-criteria/query/find-users-by-criteria.query.ts
+++ apps/users/src/features/find-by-criteria/query/find-users-by-criteria.query.ts
@@ -1,6 +1,6 @@
 import { User } from 'apps/users/src/infrastructure/entity/User.entity';
 
-export type UserCriteria = Partial<Pick<User, 'email' | 'id' | 'verified'>>;
+export type UserCriteria = Partial<Pick<User, 'email' | 'id'>>;
 
 export class FindUserByCriteriaQuery {
   constructor(public readonly criteria: UserCriteria) {}
--- apps/users/src/users-query.service.ts
+++ apps/users/src/users-query.service.ts
@@ -4,6 +4,7 @@ import { ResponseUserDto } from 'apps/libs/Users/dto/user/response-user.dto';
 import { DataSource } from 'typeorm';
 import { User } from './infrastructure/entity/User.entity';
 import { ResponseLoginDto } from 'apps/libs/Users/dto/user/response-login.dto';
+import { UserCriteria } from './features/find-by-criteria/query/find-users-by-criteria.query';
 
 @Injectable()
 export class UsersQueryService {
@@ -18,6 +19,14 @@ export class UsersQueryService {
     const queryRunner = this.dataSource.createQueryRunner();
     return await this.userQueryRepository.userLoginQuery(
       email,
+      queryRunner.manager,
+    );
+  }
+
+  async findUserByCriteria(criteria: UserCriteria): Promise<ResponseUserDto> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    return await this.userQueryRepository.findUserByCriteria(
+      criteria,
       queryRunner.manager,
     );
   }
