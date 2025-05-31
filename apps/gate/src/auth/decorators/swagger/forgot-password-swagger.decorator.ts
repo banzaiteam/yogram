8bea93392a92cbc47f81d3af31ff8d01f621e43b
Ivan Ivanov <backtobackend@proton.me>
2025-05-30 17:39:13 -0700

    BAN-31 feat: add swagger decorators to routes

---
+++ apps/gate/src/auth/decorators/swagger/forgot-password-swagger.decorator.ts
@@ -0,0 +1,28 @@
+import { applyDecorators, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+export function ForgotPasswordSwagger() {
+  return applyDecorators(
+    ApiResponse({
+      status: 200,
+      description: 'email was sent succesfully',
+    }),
+    ApiResponse({
+      status: 401,
+      description: 'user with this email was not found',
+    }),
+    ApiResponse({
+      status: 400,
+      description: 'user was not verified',
+    }),
+    ApiResponse({
+      status: 500,
+      description: 'forgot password email was not sent',
+    }),
+    ApiOperation({
+      summary: 'Send forgotPassword email to the user email',
+      description: 'call when user entered email on forgotPassword page',
+    }),
+    HttpCode(HttpStatus.OK),
+  );
+}
---
+++ apps/gate/src/auth/decorators/swagger/restore-password-swagger.decorator.ts
@@ -0,0 +1,24 @@
+import { applyDecorators, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+export function RestorePasswordSwagger() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'save new password and redirect to the login page',
+      description: 'call when user entered new password',
+    }),
+    ApiResponse({
+      status: 200,
+      description: 'user`s password was updated successfully',
+    }),
+    ApiResponse({
+      status: 500,
+      description: 'user password was not updated',
+    }),
+    ApiResponse({
+      status: 404,
+      description: 'user was not found',
+    }),
+    HttpCode(HttpStatus.OK),
+  );
+}
