1eda82370b5123571d4018156344a50fa61dbaed
idogmat <idogmat@gmail.com>
2025-06-06 15:52:15 +0300

    fix

--- apps/posts/src/features/posts/use-cases/create-post.ts
+++ apps/posts/src/features/posts/use-cases/create-post.ts
@@ -1,5 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PostRepository } from '../infrastracture/repository/post.repository';
+import { Post } from '../infrastracture/entity/post.entity';
 
 export class CreatePostCommand {
   constructor(
@@ -11,9 +12,9 @@ export class CreatePostCommand {
 export class CreatePostUseCase implements ICommandHandler<CreatePostCommand> {
   constructor(private postsRepository: PostRepository) { }
 
-  async execute(command: CreatePostCommand): Promise<string> {
+  async execute(command: CreatePostCommand): Promise<Post> {
     const newPost = await this.postsRepository.create(command);
 
-    return newPost.id;
+    return newPost;
   }
 }
