35c8b12c32b38d9cf100deaf2347a4c6a1b8ff27
idogmat <idogmat@gmail.com>
2025-06-06 13:48:17 +0300

    fix

--- apps/libs/Posts/constants/path.enum.ts
+++ apps/libs/Posts/constants/path.enum.ts
@@ -1,9 +1,3 @@
 export enum HttpPostsPath {
   Create = 'posts/create',
-  CreateWithGoogle = 'users/google',
-  FindUserByCriteria = 'users',
-  FindUserByProviderId = 'users/findone',
-  FindProviderByProviderId = 'users/provider',
-  EmailVerify = 'users/email-verify',
-  UpdateUserByCriteria = 'users/update',
 }
