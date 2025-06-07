88dd5d362a58a89177723cc0ae54c652cd5cd4fd
Ivan Ivanov <backtobackend@proton.me>
2025-06-06 17:11:07 -0700

    BAN-32 feat: add logout

--- apps/gate/src/auth/guards/refresh.guard.ts
+++ apps/gate/src/auth/guards/refresh.guard.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   CanActivate,
   ExecutionContext,
   Injectable,
@@ -7,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { SessionProvider } from '../session/session.provider';
-import { Device } from '../session/types/device.type';
 import { UsersService } from '../../users/users.service';
 
 @Injectable()
@@ -27,28 +25,24 @@ export class RefreshGuard implements CanActivate {
     try {
       // if token is dead we delete session and throw unauthorized
       payload = await this.sessionProvider.verifyToken(refreshToken);
-      console.log('🚀 ~ RefreshGuard ~ canActivate ~ payload:', payload);
     } catch (err) {
-      console.log('🚀 ~ RefreshGuard ~ canActivate ~ err:', err);
+      console.log('🚀 ~ RefreshGuard ~ err:', err);
       if (err instanceof TokenExpiredError) {
         await this.sessionProvider.deleteSession(refreshToken);
         throw new UnauthorizedException('RefreshGuard: expired/invalid token');
       }
     }
+    // check if user is alive
     const user = await this.usersService.findUserByCriteria({
       id: payload['id'],
     });
     if (!user)
       throw new UnauthorizedException('RefreshGuard: user was not found');
-    //if !session or !active -> session does not exists or unactive, maybe fraud after user logged out and try to enter with stolen, still live token
+    //if !session or !active -> session does not exists or unactive, maybe fraud after user logged out and try to enter with stolen, still alive token
     const deviceSession =
       await this.sessionProvider.findSessionbyToken(refreshToken);
-    console.log(
-      '🚀 ~ RefreshGuard ~ canActivate ~ deviceSession:',
-      deviceSession,
-    );
-    if (!deviceSession || !deviceSession['active']) {
-      throw new BadRequestException('session does not exists or unactive');
+    if (!deviceSession || !!deviceSession['active']) {
+      throw new UnauthorizedException('session does not exists or unactive');
     }
     request.user = payload;
     return true;
