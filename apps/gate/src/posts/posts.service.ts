019a48d399e2220a6e98563bce5043ef378f93fb
Ivan Ivanov <backtobackend@proton.me>
2025-06-08 14:27:22 -0700

    BAN-32 test fix

--- apps/gate/src/posts/posts.service.ts
+++ apps/gate/src/posts/posts.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { GateService } from '../../../libs/gateService';
 import { HttpPostsPath } from '../../../libs/Posts/constants/path.enum';
 import { CreatePostDto } from '../../../libs/Posts/dto/input/create-post.dto';
-import { HttpServices } from 'apps/gate/common/constants/http-services.enum';
+import { HttpServices } from '../../../../apps/gate/common/constants/http-services.enum';
 
 @Injectable()
 export class PostsService {
