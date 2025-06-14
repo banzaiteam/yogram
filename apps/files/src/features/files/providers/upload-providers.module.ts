718b3ac78928c60388172daf3de9111ba08bec30
Ivan Ivanov <backtobackend@proton.me>
2025-06-13 23:27:15 -0700

    BAN-42 fix: test

--- apps/files/src/features/files/providers/upload-providers.module.ts
+++ apps/files/src/features/files/providers/upload-providers.module.ts
@@ -3,8 +3,10 @@ import { IUploader } from './interface/uploader.interface';
 import { UploadProvidersFactory } from './upload-providers-factory';
 import { AwsService } from './aws/aws.service';
 import { GoogleService } from './google/google.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     UploadProvidersFactory,
     AwsService,
