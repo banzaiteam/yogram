b6e4d9d23cff6dc909fb2c35e112e817529c6106
Ivan Ivanov <backtobackend@proton.me>
2025-10-01 21:39:41 -0700

    dfsdfds

--- apps/files/src/features/files/providers/aws/aws.service.ts
+++ apps/files/src/features/files/providers/aws/aws.service.ts
@@ -32,6 +32,7 @@ export class AwsService implements IUploader {
   constructor(private readonly configService: ConfigService) {}
 
   async uploadFiles(file: ChunkedFileDto): Promise<UploadFilesResponse> {
+    console.log('🚀 ~ AwsService ~ uploadFiles ~ file:', file);
     const bucketName = file.bucketName;
     const isBucketExists = await this.isBucketExists(
       bucketName,
--- apps/users/src/features/update/command/update-user-by-criteria.handler.ts
+++ apps/users/src/features/update/command/update-user-by-criteria.handler.ts
@@ -4,6 +4,7 @@ import { UsersCommandService } from '../../../../../../apps/users/src/users-comm
 import { InternalServerErrorException } from '@nestjs/common';
 import { ResponseUserDto } from 'apps/libs/Users/dto/user/response-user.dto';
 import { ConfigService } from '@nestjs/config';
+import { AwsBuckets } from 'apps/libs/Files/constants/aws-buckets.constant';
 
 @CommandHandler(UpdateUserByCriteriaCommand)
 export class UpdateUserByCriteriaHandler
@@ -25,7 +26,7 @@ export class UpdateUserByCriteriaHandler
     const updatedUser = await this.usersCommandService.updateUser(
       criteria,
       updateUserDto,
-      this.configService.get('BUCKET'),
+      AwsBuckets.Files,
       file,
     );
     if (!updatedUser)
