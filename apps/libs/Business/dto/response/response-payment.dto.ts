1d4c318c41170c47c10568dce7451de79a5afd80
backtobackend226 <backtobackend@proton.me>
2026-01-25 13:04:07 -0800

    add subsription to payemntDto

--- apps/libs/Business/dto/response/response-payment.dto.ts
+++ apps/libs/Business/dto/response/response-payment.dto.ts
@@ -1,7 +1,10 @@
 import { Expose } from 'class-transformer';
 import { Payment } from '../../../../../apps/business/src/infrastructure/entity/payment.entity';
+import { Subscription } from 'apps/business/src/infrastructure/entity/subscription.entity';
 
 export class ResponsePaymentDto extends Payment {
   @Expose()
   expiresAt: Date;
+  @Expose()
+  subscription: Subscription;
 }
